a91739202a09425ae22f29550b49055eae5b4978
pasha216 <mohammedalhbyb@gmail.com>
2025-10-23 13:11:50 +0200

    Code Sugaring

--- base/views.py
+++ base/views.py
@@ -73,8 +73,8 @@ def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q))
     topics = Topic.objects.all()[0:5]
-    room_messages= Message.objects.filter(Q(room__topic__name__icontains=q))
-    rooms_count = rooms.count()
+    room_messages= Message.objects.filter(Q(room__topic__name__icontains=q)| Q(room__name__icontains=q))
+    rooms_count = rooms.count() 
     topics_count = Room.objects.count
     context = {
         "rooms": rooms,
